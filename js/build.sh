474e1078dae349a85d5b4d7206053a7fff140dd3
Michael Graf <michael.graf@fhstp.ac.at>
2022-02-03 16:01:43 +0100

    fix: remove rollup -> make plain js, update

--- js/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-nvm use lts/*
-npm run build
-
--- src/collective/bbcodesnippets/static/collective.bbcodesnippets.js
+++ src/collective/bbcodesnippets/static/collective.bbcodesnippets.js
@@ -1,55 +1,57 @@
-(function () {
-  "use strict";
+"use strict";
+
+require(["tinymce"], function (tinymce) {
+  console.log("create and add collectivebbcodesnippets");
+  var portalUrl = document.body.dataset["portalUrl"];
+  var bbcodesnippet_enabled_url = portalUrl + "/@bbcodesnippets_enabled";
 
   // keep import for unknown reason, otherwise:
-
-  const replace = (selection, template) => {
-    const content = selection.getContent({ format: "text" });
-    let replaced = template.replace("$TEXT", content);
-    replaced.indexOf("$CURSOR");
+  var replace = function replace(selection, template) {
+    var content = selection.getContent({
+      format: "text",
+    });
+    var replaced = template.replace("$TEXT", content);
+    var position = replaced.indexOf("$CURSOR");
     replaced = replaced.replace("$CURSOR", "");
-    selection.setContent(replaced);
-    // if (position >= 0) {
+    selection.setContent(replaced); // if (position >= 0) {
     //   selection.setCursorLocation(selection, position)
     // }
   };
 
-  require(["tinymce"], function (tinymce) {
-    console.log("create and add collectivebbcodesnippets");
-    const portalUrl = document.body.dataset["portalUrl"];
-    const bbcodesnippet_enabled_url = portalUrl + "/@bbcodesnippets_enabled";
-    fetch(bbcodesnippet_enabled_url, {
-      headers: { Accept: "application/json" },
+  fetch(bbcodesnippet_enabled_url, {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
     })
-      .then((response) => response.json())
-      .then((data) => {
-        tinymce.create("tinymce.plugins.CollectiveBBCodeSnippetsPlugin", {
-          init: (editor) => {
-            editor.on("init", function () {
-              console.log("editor on init!");
+    .then(function (data) {
+      tinymce.create("tinymce.plugins.CollectiveBBCodeSnippetsPlugin", {
+        init: function init(editor) {
+          editor.on("init", function () {
+            console.log("editor on init!");
+          }); // Adds a menu item to the tools menu
+
+          data.forEach(function (entry, index) {
+            var identifier = "bbcs" + entry.name;
+            console.log(index + " " + identifier);
+            editor.addMenuItem(identifier, {
+              text: entry.name + " (" + entry.snippet + ")",
+              context: "bbcs",
+              onClick: function onClick() {
+                replace(editor.selection, entry.template);
+              },
             });
-            // Adds a menu item to the tools menu
-            data.forEach((entry, index) => {
-              const identifier = "bbcs" + entry.name;
-              console.log(index + " " + identifier);
-              editor.addMenuItem(identifier, {
-                text: entry.name + " (" + entry.snippet + ")",
-                context: "bbcs",
-                onClick: () => {
-                  replace(editor.selection, entry.template);
-                },
-              });
-            });
-          },
-        });
-        tinymce.PluginManager.add(
-          "collectivebbcodesnippets",
-          tinymce.plugins.CollectiveBBCodeSnippetsPlugin
-        );
-      })
-      .catch((err) => {
-        console.log(err);
+          });
+        },
       });
-  })();
+      tinymce.PluginManager.add(
+        "collectivebbcodesnippets",
+        tinymce.plugins.CollectiveBBCodeSnippetsPlugin
+      );
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 })();
-//# sourceMappingURL=collective.bbcodesnippets.js.map
